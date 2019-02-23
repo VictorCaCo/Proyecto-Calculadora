b9fa757d48508198a130ada0830df3d998d7d03e
VictorCaCo <47927104+VictorCaCo@users.noreply.github.com>
2019-02-23 14:33:22 -0600

    Update ProyectoCalculadora.java

--- ProyectoCalculadora.java
+++ ProyectoCalculadora.java
@@ -20,6 +20,25 @@ public class ProyectoCalculadora {
         arre= new String[MAX];
         numE=0;
     } 
+    
+    public boolean contadorParentesis(){
+        //Método para saber si hay un error de sintaxis
+        int cont=0, cont2=0;
+        boolean res=false;
+        for(int i=0; i++; cad.length()-1){
+            //recorre toda la cadena
+            if(cad.charAt(i)=='(')
+                //cuenta los parentesis abiertos
+                cont++;
+            else
+                if(cad.charAt(i)==')')
+                    //cuenta los parentesis cerrados
+                    cont2++;
+        }
+        if(cont==cont2)
+            res=true;
+        return res;
+    }
     /**
      * @param args the command line arguments
      */
